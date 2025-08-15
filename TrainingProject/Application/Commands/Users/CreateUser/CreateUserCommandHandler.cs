812786df5e66bf542413cfa18876906050ede0ca
antonjohanssoon <143598031+antonjohanssoon@users.noreply.github.com>
2025-08-15 12:30:04 +0200

    User in application implemented

---
+++ TrainingProject/Application/Commands/Users/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,63 @@
+﻿using Application.Interfaces.RepositoryInterface;
+using Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Commands.Users.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, OperationResult<User>>
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly ILogger<CreateUserCommandHandler> _logger;
+
+        public CreateUserCommandHandler(IRepository<User> userRepository, ILogger<CreateUserCommandHandler> logger)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        public async Task<OperationResult<User>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+
+            _logger.LogInformation("Handling CreateUserCommand for user: {Name}", request.NewUser.Name);
+
+            var existingUser = await _userRepository.GetByUsernameAsync(request.NewUser.Username);
+            if (existingUser.IsSuccess)
+            {
+                return OperationResult<User>.Failure("Username already taken", "Validation error.");
+            }
+
+            var userToCreate = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = request.NewUser.Name,
+                Username = request.NewUser.Username,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.NewUser.PasswordHash)
+            };
+
+
+            try
+            {
+                var result = await _userRepository.AddAsync(userToCreate);
+
+                if (result.IsSuccess)
+                {
+                    _logger.LogInformation("Successfully added a new user with username: {Username}", userToCreate.Username);
+                    return OperationResult<User>.Success(userToCreate, "User added successfully!");
+                }
+
+                _logger.LogWarning("Failed to add user with username: {Username}. Error: {ErrorMessage}",
+                       userToCreate.Username, result.ErrorMessage);
+
+                return OperationResult<User>.Failure(result.ErrorMessage, "Failed to add user.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while adding user with username: {Username}", userToCreate.Username);
+                return OperationResult<User>.Failure($"An error occurred: {ex.Message}", "Database error.");
+            }
+        }
+
+
+    }
+}
---
+++ TrainingProject/Application/DTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class UserDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Password must be between 2 and 50 characters.")]
+        public string PasswordHash { get; set; }
+    }
+}
